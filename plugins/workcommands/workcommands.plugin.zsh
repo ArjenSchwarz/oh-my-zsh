a73b710c5a944815bee823c9dd0c7721f96436d1
Arjen Schwarz <developer@arjen.eu>
2014-08-07 23:15:24 +1000

    Don't loop through the dirs, but filter with find
    Now also shows a message about multiple results found

--- plugins/workcommands/workcommands.plugin.zsh
+++ plugins/workcommands/workcommands.plugin.zsh
@@ -1,20 +1,22 @@
 site() {
-    DIRS=("${(@f)$(find ~/projects -maxdepth 3 -mindepth 3 -type d -path '*/sites/*' ! -iname '.*')}")
-    for dir in $DIRS
-    do
-        if [[ $dir =~ $1 ]]; then
-            cd $dir
-        fi
-    done
+    DIRS=("${(@f)$(find ~/projects -maxdepth 3 -mindepth 3 -type d -path "*/sites/*${1}*" ! -iname ".*")}")
+    if (( ${#DIRS} == 1 )); then
+        cd ${DIRS[1]}
+    else
+        echo 'Multiple sites found:'
+        print -C 1 $DIRS
+        cd ${DIRS[1]}
+    fi
 }
 
 serv() {
-    DIRS=("${(@f)$(find ~/projects -maxdepth 3 -mindepth 3 -type d -path '*/servers/*' ! -iname '.*')}")
-    for dir in $DIRS
-    do
-        if [[ $dir =~ $1 ]]; then
-            cd $dir
-        fi
-    done
+    DIRS=("${(@f)$(find ~/projects -maxdepth 3 -mindepth 3 -type d -path "*/servers/*${1}" ! -iname ".*")}")
+    if (( ${#DIRS} == 1 )); then
+        cd ${DIRS[1]}
+    else
+        echo 'Multiple servers found:'
+        print -C 1 $DIRS
+        cd ${DIRS[1]}
+    fi
 }
 
