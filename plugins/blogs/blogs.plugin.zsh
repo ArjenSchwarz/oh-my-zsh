83a999141169c5726ba70b2fe7c762c5471f37a9
Arjen Schwarz <developer@arjen.eu>
2014-08-09 23:12:43 +1000

    Added some functions for the blog cloudformation template

--- plugins/blogs/blogs.plugin.zsh
+++ plugins/blogs/blogs.plugin.zsh
@@ -5,3 +5,23 @@ ignoreimage() {
   aws s3api put-object --bucket assets2.nore.me --key posts/$1 --body $1 --profile blogs;
   echo "![alttext](http://assets2.nore.me/posts/$1)" | pbcopy;
 }
+
+# Update the cloudformation stack with the default values
+blogcf() {
+  echo "Updating blog cloudformation with default values"
+  cd ~/projects/server_personal; aws cloudformation update-stack --stack-name blog-server --template-body file://cloudformation-generated.json --profile blogs
+}
+
+# Update the cloudformation stack with my current external IP address
+blogcfssh() {
+  externalip=`curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'`
+  echo "Current IP is: ${externalip}\n"
+  echo "Updating blog cloudformation template with current IP address"
+  cd ~/projects/server_personal; aws cloudformation update-stack --stack-name blog-server --template-body file://cloudformation-generated.json --parameters ParameterKey=SshIp,ParameterValue=${externalip}/32 --profile blogs
+}
+
+# Compile the cloudformation template from the DSL
+blogcfdsl() {
+  echo "Compiling blog cloudformation DSL into JSON"
+  cd ~/projects/server_personal; cfndsl Cloudformation-template.rb | json_pp > cloudformation-generated.json
+}
