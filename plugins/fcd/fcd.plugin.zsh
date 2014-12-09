9432d9ed6d75626c0fcd9210f0606e033ca72e4e
Arjen Schwarz <developer@arjen.eu>
2014-12-10 08:21:48 +1100

    Added autocompletion for fcd

--- plugins/fcd/fcd.plugin.zsh
+++ plugins/fcd/fcd.plugin.zsh
@@ -3,9 +3,7 @@
 # This even works in hierarchical directory structures such as clientname/projectname
 
 # Usage:
-# fcd filtername searchdepth
-# e.g. fcd oh-my-zsh 2 will look for any results 2 levels deep
-# Default value for searchdepth is 1
+# fcd filtername
 # Running fcd without any arguments will show a list of all directories
 
 # Customisation:
@@ -14,24 +12,24 @@
 # FCD_BASEDIR=$HOME/projects
 # You can set fcd to search in multiple directories, e.g. projects and utils by adding them as an array
 # FCD_BASEDIR=($HOME/projects $HOME/utils)
-#
-# Set up functions for your common usages. For example if you have websites configured in
-# ~/projects/clientname/sites/projectname you can search by projectname using "site projectname"
-# if you define the below function
-# site() {
-#    fcd "/sites/*${1}" 3
-# }
+# More complex structures are possible too. For example if your projects are organized by client or even by client and project type
+# FCD_BASEDIR=($HOME/projects/*/sites)
 
 # Set the default basedir to projects
 [[ -z $FCD_BASEDIR ]] && FCD_BASEDIR=$HOME/projects
 
-# Expects two parameters, the searchterm and the searchdepth. Searchdepth is 1 by default
+
+_fcd_get_list() {
+    DIRS=("${(@f)$(find ${FCD_BASEDIR} -mindepth 1 -maxdepth 1 -type d -path "*$1*" ! -iname ".*")}")
+    print -C 1 $DIRS | awk '{gsub(/\/.*\//,"",$1); print}'
+}
+
+_fcd() {
+   compadd `_fcd_get_list`
+}
+
 fcd() {
-    depth=1
-    if (($+2)); then
-        depth="${2}"
-    fi
-    DIRS=("${(@f)$(find ${FCD_BASEDIR} -mindepth $depth -maxdepth $depth -type d -path "*${1}*" ! -iname ".*")}")
+    DIRS=("${(@f)$(find ${FCD_BASEDIR} -mindepth 1 -maxdepth 1 -type d -path "*$1*" ! -iname ".*")}")
     if (( ${#DIRS} == 1 )); then
         cd ${DIRS[1]}
     else
@@ -40,3 +38,5 @@ fcd() {
         cd ${DIRS[1]}
     fi
 }
+
+compdef _fcd fcd
